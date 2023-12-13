c738e1878928a2b16d642a3770343e3661e08ac2
Raoul J.P. Bonnal <ilpuccio.febo@gmail.com>
2023-12-13 14:52:57 +0100

    Create init-random.sh

---
+++ init-random.sh
@@ -0,0 +1,30 @@
+# 20 large <2GB each , 20 medium <200MB each, 20 small <8MB each, 20 smallest <1MB each
+#large files
+mkdir -p data
+cd data
+mkdir -p transcoding-files
+cd transcoding-files
+for n in {1..20}; do
+  dd if=/dev/urandom of=file$( printf %03d "$n" ).bin bs=64k count=$(( RANDOM + 1024 ))
+done
+#small files
+cd ..
+mkdir -p rendering-files
+cd rendering-files
+for n in {1..20}; do
+  dd if=/dev/urandom of=file$( printf %03d "$n" ).bin bs=1 count=$(( RANDOM + 1024 ))
+done
+#medium files
+cd ..
+mkdir -p image-files
+cd image-files
+for n in {1..20}; do
+    dd if=/dev/urandom of=file$( printf %03d "$n" ).bin bs=8k count=$(( RANDOM + 1024 ))
+done
+cd ..
+mkdir -p project-files
+cd project-files
+for n in {1...20}; do
+  dd if=/dev/urandom of=file$( printf %03d "$n$" ).bin bs=4k count=$(( RANDOM +1024 ))
+done
+cd ..
